628c929d5b25531be354a749a4e75d7fcf30379f
ILilliasI <agsukhova_1@edu.hse.ru>
2023-02-27 13:29:08 +0300

    Add top-100 repos info functionality

--- sim_dev_search/main.py
+++ sim_dev_search/main.py
@@ -77,6 +77,29 @@ def cli():
     Similar developers search line tool.
     """
 
+@click.command("top")
+@click.option("-r", "--repos_list", multiple=True, default=["https://github.com/pytorch/pytorch"],
+              help="Provide paths to Github repositories.")
+def stargazers_top(repos_list: List[str]) -> None:
+    """
+    Get top 100 GitHub repos in popularity among stargazers.
+    Data is saved to results/repositories_top.json.
+
+    :param repos_list: List of paths to GitHub repositories.
+    """
+    info_extractor = StargazersTopExtractor(repos_list)
+    path_to_result = Path(__file__).parent.parent / "results" / "repositories_top.json"
+
+    with open(path_to_result, "w") as fp:
+        json.dump(info_extractor.repositories_top, fp, indent=8, sort_keys=True)
+
+
+@click.group()
+def cli():
+    """
+    Similar developers search line tool.
+    """
+
 
 if __name__ == "__main__":
     cli.add_command(programmers_info)
