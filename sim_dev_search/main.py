50aa2cd22b0ed7178c8c971b13777b64d54f2d7f
ILilliasI <agsukhova_1@edu.hse.ru>
2023-04-01 20:08:48 +0300

    Refactoring

--- sim_dev_search/main.py
+++ sim_dev_search/main.py
@@ -1,26 +1,46 @@
+from pathlib import Path
 from typing import List
 
-import click
 import json
+import click
+import io
 
 from utils.repos_info_extractor import ReposInfoExtractor
 
 
-@click.command()
-@click.option("-r", "--repos_list", multiple=True, default=["https://github.com/ishepard/pydriller"],
-              help="Provide paths to Github repositories.")
-def run(repos_list: List[str]) -> None:
+@click.command(name="prog")
+@click.option(
+    "-r",
+    "--repos_list",
+    multiple=True,
+    default=["https://github.com/ishepard/pydriller"],
+    help="Provide paths to Github repositories.",
+)
+@click.option(
+    "-p",
+    "--file-out",
+    default=click.open_file(str(Path(__file__).absolute().parent.parent / "results" / "programmers_commits.json"), "w"),
+    type=click.File("w"),
+    help="Provide path to save result.",
+)
+def programmers_info(repos_list: List[str], file_out: io.TextIOWrapper) -> None:
     """
+    Get information about developers and their commits.
 
-    :param repos_list:
-    :return:
+    :param repos_list: List of paths to GitHub repositories.
+    :param file_out: file to write results.
     """
     info_extractor = ReposInfoExtractor(repos_list)
-    path_to_result = 'results/programmers_commits.json'
-
-    with open(path_to_result, 'w') as fp:
-        json.dump(info_extractor.programmers_info, fp, indent=8, sort_keys=True)
+    json.dump(info_extractor.programmers_info, file_out, indent=8, sort_keys=True)
 
 
-if __name__ == '__main__':
-    run()
+@click.group()
+def cli():
+    """
+    Similar developers search line tool.
+    """
+
+
+if __name__ == "__main__":
+    cli.add_command(programmers_info)
+    cli()
--- sim_dev_search/utils/repos_info_extractor.py
+++ sim_dev_search/utils/repos_info_extractor.py
@@ -16,7 +16,7 @@ class ReposInfoExtractor:
 
     def _extract_repo_info(self, path_to_repo: str) -> None:
         """
-        Extract info about programmers and their commits.
+        Extract info about developers and their commits.
         :param path_to_repo: Path to GitHub repository.
         """
         commits_count = 0
@@ -26,31 +26,26 @@ class ReposInfoExtractor:
         for commit in tqdm(Repository(path_to_repo).traverse_commits(),
                            total=commits_count, desc=f"Extracting from {path_to_repo}"):
             author_id = commit.author.email
-            commits_field_name = commit.author.name + "_commits"
-
-            if commits_field_name not in self._programmers_info[author_id]:
-                self._programmers_info[author_id][commits_field_name] = {}
 
             for file in commit.modified_files:
-                self._add_file_info(author_id, commits_field_name, file)
+                self._add_file_info(author_id, file)
 
-    def _add_file_info(self, author_id: str, commits_field_name: str, file: ModifiedFile) -> None:
+    def _add_file_info(self, author_id: str, file: ModifiedFile) -> None:
         """
-        Add information about modified file to programmer information.
-        :param author_id: Unique name of GitHub programmer.
-        :param commits_field_name: Name of commit author.
+        Add information about modified file to developer information.
+        :param author_id: Unique name of GitHub developer.
         :param file: File from GitHub repository.
         """
-        if file.filename not in self._programmers_info[author_id][commits_field_name]:
-            self._programmers_info[author_id][commits_field_name][file.filename] = {"added": 0, "deleted": 0}
-        self._programmers_info[author_id][commits_field_name][file.filename]["added"] += file.added_lines
-        self._programmers_info[author_id][commits_field_name][file.filename]["deleted"] += file.deleted_lines
+        if file.filename not in self._programmers_info[author_id]:
+            self._programmers_info[author_id][file.filename] = {"added": 0, "deleted": 0}
+        self._programmers_info[author_id][file.filename]["added"] += file.added_lines
+        self._programmers_info[author_id][file.filename]["deleted"] += file.deleted_lines
 
     @property
     def programmers_info(self) -> Dict[str, dict]:
         """
-        Information about programmers and their commits from given repositories.
-        :return: dictionary of programmers and their commits.
+        Information about developers and their commits from given repositories.
+        :return: dictionary of developers and their commits.
         """
         if len(self._programmers_info) != 0:
             return self._programmers_info
